f9afc851429972fbbf235e1971c24622f4f21d6d
Ivan <zadorohhnyiv@gmail.com>
2023-11-08 00:31:24 +0200

    begun

---
+++ main.py
@@ -0,0 +1,26 @@
+def caesar_shift(char, shift):
+
+
+def caesar_cipher(text, key, operation):
+
+
+
+def caesar_encrypt(text, key):
+
+
+
+def caesar_decrypt(text, key):
+
+
+def read_file(path):
+
+
+def write_file(path, text):
+
+
+def process_file(read_path, write_path, key, operation):
+
+
+def main():
+    if __name__ == "__main__":
+        main()
